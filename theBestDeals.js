96cd15c137b059aa8e877311fe6c46f5ab101590
Dyllon <isaacsdyllon123@gmail.com>
2017-05-10 16:14:44 +0200

    code that test if quantity and price is split

--- theBestDeals.js
+++ theBestDeals.js
@@ -1,30 +1,5 @@
-// var totalPhoneBill = function(phoneUsage) {
-//     var splitPhoneUsage = phoneUsage.split(',');
-//
-//     const costPerCall = 2.75;
-//     const costSms = 0.65;
-//
-//     var listOfUsage = [];
-//     var call = [];
-//     var sms = [];
-//
-//     for (var x = 0; x < splitPhoneUsage.length; x++) {
-//         listOfUsage.push(splitPhoneUsage[x]);
-//
-//         if (listOfUsage[x].includes('sms')) {
-//             sms.push(listOfUsage['sms']);
-//         } else if (listOfUsage[x].includes('call')) {
-//             call.push(listOfUsage['call']);
-//         }
-//     }
-//
-//     var totalPhoneBill = (sms.length * costSms) + (call.length * costPerCall);
-//
-//     return 'R' + totalPhoneBill;
-// }
-//
-// console.log(totalPhoneBill('call,sms,call,sms,sms'));
 var quantity = ['1','2','3','5',];
+
 var price = ['3','7','10','14.50'];
 
 
